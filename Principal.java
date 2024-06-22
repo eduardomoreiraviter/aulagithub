667ada30a68e21e78c62b8acf3a6b3d837ebf519
Eduardo Moreira Viter <moreiraviter@outlook.com>
2024-06-22 13:41:18 -0300

    adicionando um commit

--- Principal.java
+++ Principal.java
@@ -1,7 +1,9 @@
 public class Principal {
     public static void main(String[] args) {
         //println
-        System.out.println("Teste GitHub curso DevSuperio!");
+        System.out.println("Teste GitHub curso DevSuperior!");
+        //segundo commit
+        System.out.println("Segundo commit.");
 
     }
 }
